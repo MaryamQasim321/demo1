f6f8fc180f297c0bf5a51892b8965b7039e615cf
Maryam Qasim <maryam.qasim.mq1@gmail.com>
2025-07-15 15:57:08 +0500

    pushed SQL code

--- src/main/java/org/example/demo1/DatabaseConnector.java
+++ src/main/java/org/example/demo1/DatabaseConnector.java
@@ -1,13 +1,9 @@
 package org.example.demo1;
-
 import java.sql.*;
-
 public class DatabaseConnector {
-
     String jdbcURL = "jdbc:mysql://localhost:3306/retailSystem";
     String username = "root";
     String password = "BSCSBatch2027";
-
     public DatabaseConnector() {
         try (
                 Connection conn = DriverManager.getConnection(jdbcURL, username, password);
@@ -19,12 +15,10 @@ public class DatabaseConnector {
             while (rs.next()) {
                 System.out.println(rs.getInt("productId") + " - " + rs.getString("name"));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) {
         new DatabaseConnector();  // call the constructor
     }
